f490520940f4aa6f39bcb754f33dd77d7db4d402
Brahim Akarouch <brahim.akarouch@gmail.com7>
2021-02-25 14:27:03 +0100

    Send sources to 3rd API

--- functions/sendToSourcesAPI.js
+++ functions/sendToSourcesAPI.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 exports.sendToSourcesAPI = async (categoryTranslated, mediaLogo, mediaName) => {
 	axios
-		.post('http://localhost:3006/api/v1/sources', {
+		.post('https://postgoo-final-api.herokuapp.com/api/v1/sources', {
 			sourcesLanguage: 'ar',
 			category: categoryTranslated,
 			media: [
